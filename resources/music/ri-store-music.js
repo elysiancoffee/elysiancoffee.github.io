077fcb7f4b3f57a41b2a1074b644c9d8657115ab
elysiancoffee <158056163+elysiancoffee@users.noreply.github.com>
2025-07-19 02:56:33 -0700

    Update ri-store-music.js

--- resources/music/ri-store-music.js
+++ resources/music/ri-store-music.js
@@ -40,10 +40,10 @@
 
     if (holdUntil && now < parseInt(holdUntil)) {
       audio.pause();
-      bubble.innerHTML = '▶️';
+      bubble.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-play-icon lucide-play"><polygon points="6 3 20 12 6 21 6 3"/></svg>';
     } else {
       audio.play();
-      bubble.innerHTML = '⏸️';
+      bubble.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-pause-icon lucide-pause"><rect x="14" y="4" width="4" height="16" rx="1"/><rect x="6" y="4" width="4" height="16" rx="1"/></svg>';
     }
   }
 
